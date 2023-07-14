d2c8e6209f6c65b832ee4aa21e5db2d9c669d11f
GewoonLeon <gruttersleonbot2@gmail.com>
2023-07-14 22:21:19 +0200

    fixed issue hopefully

--- scripts/craft.lua
+++ scripts/craft.lua
@@ -7,16 +7,15 @@ a, blockData = turtle.inspect()
 
 while true do
     while blockData ~= "minecraft:chest" do
+        a, blockData = turtle.inspect()
         turtle.turnLeft()
         sleep(0)
     end
     for i = 1, #craftingSlots do
         turtle.select(craftingSlots[i])
-        currentSlotAmount = turtle.getItemCount()
-        currentSlotSpace = turtle.getItemSpace()
-        while currentSlotSpace ~= 0 do
-            turtle.suck(currentSlotAmount - currentSlotSpace)
-        end  
+        while turtle.getItemSpace() ~= 0 do
+            turtle.suck(turtle.getItemSpace() - turtle.getItemCount())
+        end  l
     end 
     turtle.select(16)
     turtle.craft()
